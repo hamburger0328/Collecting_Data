803363739e0fe624109d2775c1da4208d7986007
hamburger0328 <seohun8340@gmail.com>
2023-09-07 00:35:33 +0900

    React에 대한 협업 노하우 전수

---
+++ src/components/buttonData/toggleBtn/ToggleBtn.jsx
@@ -0,0 +1,18 @@
+/**
+ * @hamburger0328
+ * @param {ToggleBtn} function 토글버튼
+ * @brief 스위치 토글 버튼 함수
+ * @author 박서희
+ * @date 23.09.07
+ */
+import React from "react";
+
+const ToggleBtn = () => {
+  return (
+    <React.Fragment>
+      <div className="">ToggleBtn</div>
+    </React.Fragment>
+  );
+};
+
+export default ToggleBtn;
