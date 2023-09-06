abc5e8bb10ab5750fa01e6bab2aab5910bce00a5
hamburger0328 <seohun8340@gmail.com>
2023-09-07 00:40:43 +0900

    React에 대한 협업 노하우 전수

--- src/components/buttonData/toggleBtn/ToggleBtn.jsx
+++ src/components/buttonData/toggleBtn/ToggleBtn.jsx
@@ -1,8 +1,15 @@
 /**
  * @hamburger0328
+ * ▼ param = 함수설명을 적는 곳
  * @param {ToggleBtn} function 토글버튼
+ *
+ * ▼ brief = 간결한 설명 적는곳
  * @brief 스위치 토글 버튼 함수
+ *
+ * ▼ author = 작성자 적는 곳
  * @author 박서희
+ *
+ * ▼ date = 날짜 적는 곳
  * @date 23.09.07
  */
 import React from "react";
